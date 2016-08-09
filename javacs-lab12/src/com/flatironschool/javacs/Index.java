0a2a912d283095e5f299371d1420e75b347b4076
alinewm <alinewada@gmail.com>
2016-08-09 14:50:48 -0700

    Oops forgot files

---
+++ javacs-lab12/src/com/flatironschool/javacs/Index.java
@@ -0,0 +1,17 @@
+package com.flatironschool.javacs;
+
+import java.util.Map;
+import java.util.Set;
+
+import org.jsoup.select.Elements;
+
+public interface Index {
+	public boolean isIndexed(String url);
+//	public void add(String term, TermCounter tc);
+	public Set<String> getURLs(String term);
+	public Map<String, Integer> getCounts(String term);
+	public Map<String, Integer> getCountsFaster(String term);
+	public Integer getCount(String url, String term);
+	public void indexPage(String url, Elements paragraphs);
+	public void printIndex();
+}
---
+++ javacs-lab12/src/com/flatironschool/javacs/ListIndex.java
@@ -0,0 +1,131 @@
+package com.flatironschool.javacs;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.select.Elements;
+public class ListIndex implements Index{
+	
+	String fileName;
+	HashMap<String, ArrayList<Entry>> index;
+	
+	public ListIndex() {
+		
+	}
+	
+	public ListIndex(String fileName) {
+		this.fileName = fileName;
+	}
+
+	@Override
+	public boolean isIndexed(String url) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void add(String term, String url) throws IOException{ //CHANGE METHOD SIGNCATURE INTERFACE	
+		// TODO Auto-generated method stub
+		WikiFetcher wf = new WikiFetcher();
+		Elements paragraphs = wf.readWikipedia(url);
+		indexPage(url, paragraphs);
+	}
+
+	@Override
+	public Set<String> getURLs(String term) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, Integer> getCounts(String term) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, Integer> getCountsFaster(String term) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer getCount(String url, String term) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void indexPage(String url, Elements paragraphs) {
+		// TODO Auto-generated method stub
+		for (Node node: paragraphs) {
+			processTree(node, url);
+		}
+	}
+	
+	public void processTree(Node root, String url) {
+		for (Node node: new WikiNodeIterable(root)) {
+			if (node instanceof TextNode) {
+				processText(((TextNode) node).text(), url);
+			}
+		}
+	}
+	
+	public void processText(String text, String url) {
+		// replace punctuation with spaces, convert to lower case, and split on whitespace
+		String[] array = text.replaceAll("\\pP", " ").toLowerCase().split("\\s+");
+		
+		for (int i=0; i<array.length; i++) {
+			String term = array[i];
+			incrementTermCount(term, url);
+		}
+	}
+	
+	public void incrementTermCount(String term, String url) {
+		if (index.containsKey(term)) {
+			ArrayList<Entry> entryList = index.get(term);
+			for (Entry entry : entryList) {
+				if (entry.url.equals(url)) {
+					entry.addOne();
+				}
+			}
+		}
+	}
+
+	@Override
+	public void printIndex() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main(String[] args) throws IOException{
+		ListIndex listIndex = new ListIndex();
+		listIndex.index = new HashMap<String, ArrayList<Entry>>();
+		ArrayList<Entry> entries = new ArrayList<Entry>();
+		entries.add(new Entry("abc", 1));
+		listIndex.index.put("a", entries);
+		listIndex.incrementTermCount("a", "abc");
+		
+		ArrayList<Entry> list = listIndex.index.get("a");
+		System.out.println("count: " +  list.get(0).count);
+	}
+
+}
+
+class Entry {
+	String url;
+	int count;
+	
+	public Entry(String url, int count) {
+		this.url = url;
+		this.count = count;
+	}
+	
+	public void addOne() {
+		count += 1;
+	}
+}
