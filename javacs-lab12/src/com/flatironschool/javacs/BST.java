a11b586318f0bab01823c817774f8bdd05910681
bramanudom <bramanud@wellesley.edu>
2016-08-10 04:26:38 -0700

    implemented BSTIndex

--- javacs-lab12/src/com/flatironschool/javacs/BST.java
+++ javacs-lab12/src/com/flatironschool/javacs/BST.java
@@ -10,81 +10,6 @@ package com.flatironschool.javacs;
 
 import java.util.*;
 
-<<<<<<< HEAD
-public class BSTIndex {
-	
-	HashMap<String, BST> index = new HashMap<String, BST>();
-	public String[] topFive(String term) {
-	   BST termTree = index.get(term);
-	   return null;
-  }
-}
-
-class BST <Node extends Comparable<Node>> implements Iterable<Node>
-{
-  public static void main(String[] args)
-  {
-     Integer[] a = {1,5,2,7,4};
-     BST<Integer> bst = new BST<Integer>();
-     for(Integer n : a) bst.insert(n);
-
-     bst.preOrderTraversal();
-     System.out.println();
-
-     //testing comparator
-     //build a mirror BST with a rule:  Left > Parent > Right
-     //code for the comparator at the bottom of the file
-     // bst = new BST<Integer>(new MyComp1());
-     // for(Integer n : a) bst.insert(n);
-
-     // bst.preOrderTraversal();
-     // System.out.println();
-     // bst.inOrderTraversal();
-     // System.out.println();
-
-
-     // for(Integer n : bst) System.out.print(n);
-     // System.out.println();
-
-     // System.out.println(bst);
-
-     //testing restoring a tree from two given traversals
-     // bst.restore(new Integer[] {11,8,6,4,7,10,19,43,31,29,37,49},
-     //                 new Integer[] {4,6,7,8,10,11,19,29,31,37,43,49});
-     // bst.preOrderTraversal();
-     // System.out.println();
-     // bst.inOrderTraversal();
-     // System.out.println();
-
-     // //testing diameter
-     // System.out.println("diameter = " + bst.diameter());
-     // //testing width
-     // System.out.println("width = " + bst.width());
-  }
-
-
-  private Node root;
-  private Comparator<Node> comparator;
-
-  public BST()
-  {
-     root = null;
-     comparator = null;
-  }
-
-  public BST(Comparator<Node> comp)
-  {
-     root = null;
-     comparator = comp;
-  }
-
-  private int compare(Node x, Node y)
-  {
-     if(comparator == null) return x.compareTo(y);
-     else
-     return comparator.compare(x,y);
-  }
-=======
 class BST <Node extends Comparable<Node>>
 {
    public static void main(String[] args)
@@ -130,6 +55,7 @@ class BST <Node extends Comparable<Node>>
 
    private Node root;
    private Comparator<Node> comparator;
+   int size = 0;
 
    public BST()
    {
@@ -151,88 +77,40 @@ class BST <Node extends Comparable<Node>>
       else
       return comparator.compare(x,y);
    }
->>>>>>> 8e12313d0ce19f21df889f0e77113cbd1a35baad
+/*****************************************************
+*
+*            FIND
+*
+******************************************************/
+
+// modified from code snippet found on stackoverflow
+public Node findNode(String url, Node node){
+	  if(node != null){
+        if(node.name().equals(name)){
+           return node;
+        } else {
+            Node foundNode = search(name, node.left);
+            if(foundNode == null) {
+                foundNode = search(name, node.right);
+            }
+            return foundNode;
+         }
+    } else {
+        return null;
+    }
+}
+
 
 /*****************************************************
 *
 *            INSERT
 *
 ******************************************************/
-<<<<<<< HEAD
-  public void insert(int count)
-  {
-     root = insert(root, count);
-  }
 
-  private Node insert(Node p, int toInsert)
-  {
-     // System.out.println(p);
-     // System.out.println(toInsert);
-     if (p == null)
-        return new Node(toInsert);
-
-     if (compare(toInsert, p.count) == 0)
-     	return p;
-
-     if (compare(toInsert, p.count) < 0)
-        p.left = insert(p.left, toInsert);
-     else
-        p.right = insert(p.right, toInsert);
-
-     return p;
-  }
-
-/*************************************************
-*
-*            toString
-*
-**************************************************/
-
-  public String toString()
-  {
-     StringBuffer sb = new StringBuffer();
-     for(Node count : this) sb.append(count.toString());
-
-     return sb.toString();
-  }
-
-/*************************************************
-*
-*            TRAVERSAL
-*
-**************************************************/
-
-  public void preOrderTraversal()
-  {
-     preOrderHelper(root);
-  }
-  private void preOrderHelper(Node r)
-  {
-     if (r != null)
-     {
-        System.out.print(r+" ");
-        preOrderHelper(r.left);
-        preOrderHelper(r.right);
-     }
-  }
-
-  public void inOrderTraversal()
-  {
-     inOrderHelper(root);
-  }
-  private void inOrderHelper(Node r)
-  {
-     if (r != null)
-     {
-        inOrderHelper(r.left);
-        System.out.print(r+" ");
-        inOrderHelper(r.right);
-     }
-  }
-=======
    public void insert(int count, String url)
    {
       root = insert(root, count, url);
+      size++;
    }
 
    private Node insert(Node p, int newNodeCount, String newNodeUrl)
@@ -300,7 +178,7 @@ class BST <Node extends Comparable<Node>>
          inOrderHelper(r.right);
       }
    }
->>>>>>> 8e12313d0ce19f21df889f0e77113cbd1a35baad
+
 
 
 /*****************************************************
@@ -309,88 +187,6 @@ class BST <Node extends Comparable<Node>>
 *
 ******************************************************/
 
-<<<<<<< HEAD
-  public Iterator iterator()
-  {
-     return new MyIterator();
-  }
-  //pre-order
-  private class MyIterator implements Iterator<Node>
-  {
-     Stack<Node> stk = new Stack<Node>();
-
-     public MyIterator()
-     {
-        if(root != null) stk.push(root);
-     }
-     public boolean hasNext()
-     {
-        return !stk.isEmpty();
-     }
-
-     public Node next()
-     {
-        Node cur = stk.peek();
-        if(cur.left != null)
-        {
-           stk.push(cur.left);
-        }
-        else
-        {
-           Node tmp = stk.pop();
-           while( tmp.right == null )
-           {
-              if(stk.isEmpty()) return cur;
-              tmp = stk.pop();
-           }
-           stk.push(tmp.right);
-        }
-
-        return cur;
-     }//end of next()
-
-     public void remove()
-     {
-
-     }
-  }//end of MyIterator
-  class Node
-  {
-     public Node left, right;
-     int count;
-     String url;
-
-     public Node(String url, int count, Node l, Node r)
-     {
-        left = l; right = r;
-        this.url = url;
-        this.count = count;
-
-     }
-
-     public Node(int count, Node l, Node r)
-     {
-        left = l; right = r;
-        this.count = count;
-
-     }
-
-       public Node(int count)
-     {
-        this(null, count, null, null);
-     }
-
-     public Node(String url, int count)
-     {
-        this(url, count, null, null);
-     }
-
-     public String toString()
-     {
-        return ("url: " + url + " occurence: " + count);
-     }
-  } //end of Node
-=======
 //   public Iterator<Node> iterator()
 //   {
 //      return new MyIterator();
@@ -435,11 +231,19 @@ class BST <Node extends Comparable<Node>>
 //
 //      }
 //   }//end of MyIterator
+
+/*****************************************************
+*
+*            the Node class
+*
+******************************************************/
+
    class Node
    {
       public Node left, right;
       int count;
       String url;
+      boolean visited = fal
 
       public Node(String url, int count, Node l, Node r)
       {
@@ -471,28 +275,16 @@ class BST <Node extends Comparable<Node>>
          return ("url: " + url + " occurence: " + count);
       }
    } //end of Node
->>>>>>> 8e12313d0ce19f21df889f0e77113cbd1a35baad
 
-/*****************************************************
-*
-*            the Node class
-*
-******************************************************/
+
 }//end of BST
 
 
 
-class MyComp1 implements Comparator<Integer>
-{
-<<<<<<< HEAD
-  public int compare(Integer x, Integer y)
-  {
-       return y-x;
-  }
-=======
-   public int compare(Integer x, Integer y)
+class MyComp1 implements Comparator<Integer>{
+public int compare(Integer x, Integer y)
    {
         return y-x;
    }
->>>>>>> 8e12313d0ce19f21df889f0e77113cbd1a35baad
+
 }
--- javacs-lab12/src/com/flatironschool/javacs/BSTIndex.java
+++ javacs-lab12/src/com/flatironschool/javacs/BSTIndex.java
@@ -1,11 +1,130 @@
 package com.flatironschool.javacs;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.Scanner;
+import java.io.File;
+
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.select.Elements;
 
 public class BSTIndex {
 	HashMap<String, BST> index = new HashMap<String, BST>();
+	Scanner scanny;
+	String term;
+	String fileName;
+
+	public BSTIndex(String filename){
+		this.filename = filename;
+
+	}
+
+	public static void main(String[] args){
+		BSTIndex bstIndex = new BSTIndex("urls.txt");
+		try{
+			bstIndex.add()
+		} catch (IOException e){
+			System.out.println(e);
+		}
+
+		System.out.println("There are " + bstIndex.size + " words in the index");
+		System.out.println(Array.toString(bstIndex.topFive("the"));
+	}
+
 	public String[] topFive(String term) {
 	   BST termTree = index.get(term);
-	   return null;
+	   String[] result = new String[5];
+
+
+	   for(int i=0; i < 5; i++){
+	   	Node max = findMax(termTree.root);
+	   	result[i] = max.url;
+	   }
+
+	   return result; 
    }
+
+   public Node findMax(Node node){ //pass the root
+
+   		if (node.right!=null && node.right.visited==false) {
+   			findMax(node.right);
+   		} else {
+   			node.visited = true;
+   			return node;
+   		}
+   }
+
+
+   public boolean incrementTermCount(String term, String url) {
+		
+		if (index.containsKey(term)) {
+			BST entryBST = index.get(term);
+			Node foundNode = entryBST.findNode(url, entryBST.root)
+
+			if ( foundNode != null){
+				foundNode.count++;
+			} else {
+				entryBST.insert(1, url);
+			}
+		}
+
+		else{
+			BST newTermTree = new BST();
+			newTermTree.insert(1, url);
+		}
+	}
+
+   	public void add() throws IOException{ //CHANGE METHOD SIGNCATURE INTERFACE	
+		// TODO Auto-generated method stub
+		WikiFetcher wf = new WikiFetcher();
+		URL filePath = getClass().getResource(fileName);
+		scanny = new Scanner(new File(filePath.getPath()));
+		while(scanny.hasNext()){
+			String url = scanny.nextLine();
+//			System.out.println(url);
+			Elements paragraphs = wf.fetchWikipedia(url);
+			indexPage(url, paragraphs);
+			//scanny.nextLine();
+		}
+	}
+
+	public void indexPage(String url, Elements paragraphs) {
+		// TODO Auto-generated method stub
+		for (Node node: paragraphs) {
+			processTree(node, url);
+		}
+	}
+	
+	public void processTree(Node root, String url) {
+		for (Node node: new WikiNodeIterable(root)) {
+			if (node instanceof TextNode) {
+				processText(((TextNode) node).text(), url);
+			}
+		}
+	}
+	
+	public void processText(String text, String url) {
+		// replace punctuation with spaces, convert to lower case, and split on whitespace
+		String[] array = text.replaceAll("\\pP", " ").toLowerCase().split("\\s+");
+		
+		for (int i=0; i<array.length; i++) {
+			String term = array[i];
+
+			incrementTermCount(term, url);
+		}
+	}
+
+
+
 }
